78491d2d399b31a523b679003d82fb91b931e308
Sergey <podolsknet@gmail.com>
2026-05-01 18:34:47 +0300

    Начало проекта: структура, конфиг парков, зависимости

---
+++ config/parks.py
@@ -0,0 +1,34 @@
+PARKS = [
+    {
+        "id": "fili",
+        "name": "Парк Фили",
+        "lat": 55.740384,
+        "lon": 37.441293,
+        "dry_hours": 24,
+        "start_date": None
+    },
+    {
+        "id": "erino",
+        "name": "Байк Парк Ерино",
+        "lat": 55.438771,
+        "lon": 37.504228,
+        "dry_hours": 48,
+        "start_date": None
+    },
+    {
+        "id": "chess",
+        "name": "Чесс Парк",
+        "lat": 55.567215,
+        "lon": 37.539429,
+        "dry_hours": 72,
+        "start_date": "2026-09-01"
+    },
+    {
+        "id": "kozlovka",
+        "name": "Парк Козловка",
+        "lat": 54.831991,
+        "lon": 38.125463,
+        "dry_hours": 72,
+        "start_date": "2026-06-01"
+    }
+]
